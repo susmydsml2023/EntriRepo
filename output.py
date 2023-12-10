66ab31dd4fab7df9e6406978ee7d72dafeb3f0d7
susmydsml2023 <153307821+susmydsml2023@users.noreply.github.com>
2023-12-10 18:53:51 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,104 @@
+
+
+OUTPUT IS GIVEN HERE
+
+
+C:\Users\hp\PycharmProjects\entri_project\venv\Scripts\python.exe C:\Users\hp\PycharmProjects\entri_project\BankingSystem.py 
+******Welcome to the Banking System******
+Enter your username	:Susmy
+Enter your password	:susmy123@
+Login Successsfully
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+1
+************DEPOSIT*************
+How much would you like to deposit? 200
+Rs.200 is credited to your account
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+1
+************DEPOSIT*************
+How much would you like to deposit? 200
+Rs.200 is credited to your account
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+3
+Your account balance is : Rs. 400
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+1
+************DEPOSIT*************
+How much would you like to deposit? 200
+Rs.200 is credited to your account
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+3
+Your account balance is : Rs. 600
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+2
+************WITHDRAW*************
+How much would you like to withdraw? 200
+Rs.200 is debited from your account
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+3
+Your account balance is : Rs. 400
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+4
+Enter your new password	:sus
+Re enter your password	:susmy
+Password entered do not match. Try again
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+4
+Enter your new password	:susmy123
+Re enter your password	:susmy123
+Password changed successfully!
+-----------Enter your Choice-------------
+>Press 1 for Amount Deposit
+>Press 2 for Amount Withdrawal
+>Press 3 for Account Balance
+>Press 4 for Change Password
+>Press 5 for Exit
+5
+
+Process finished with exit code 0
+  
